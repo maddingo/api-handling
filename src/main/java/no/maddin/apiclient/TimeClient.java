36d448e6939946aaaa3c69821c762a7cdcf68dda
Martin Goldhahn <martin.goldhahn@altibox.no>
2021-04-27 22:32:36 +0200

    logging

--- src/main/java/no/maddin/apiclient/TimeClient.java
+++ src/main/java/no/maddin/apiclient/TimeClient.java
@@ -1,15 +1,19 @@
 package no.maddin.apiclient;
 
+import lombok.extern.java.Log;
 import org.openapitools.client.ApiException;
 import org.openapitools.client.api.TimeControllerApi;
 
+import java.util.logging.Level;
+
+@Log
 public class TimeClient {
     public static void main(String[] args) {
 
         try {
             new TimeControllerApi().nowUTC();
         } catch (ApiException e) {
-            e.printStackTrace();
+            log.log(Level.SEVERE, "Error calling time API", e);
         }
     }
 }
