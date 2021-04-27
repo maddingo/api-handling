6d03f84267378795871f3d4a2889cf2551be7d6e
Martin Goldhahn <martin.goldhahn@altibox.no>
2021-04-27 21:05:56 +0200

    Spring client

--- src/main/java/no/maddin/apiclient/TimeClient.java
+++ src/main/java/no/maddin/apiclient/TimeClient.java
@@ -1,14 +1,14 @@
 package no.maddin.apiclient;
 
-import org.openapitools.client.ApiException;
 import org.openapitools.client.api.TimeControllerApi;
+import org.springframework.web.client.RestClientException;
 
 public class TimeClient {
     public static void main(String[] args) {
 
         try {
             new TimeControllerApi().nowUTC();
-        } catch (ApiException e) {
+        } catch (RestClientException e) {
             e.printStackTrace();
         }
     }
--- src/test/java/no/maddin/apiclient/TimeClientTest.java
+++ src/test/java/no/maddin/apiclient/TimeClientTest.java
@@ -1,28 +1,18 @@
 package no.maddin.apiclient;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openapitools.client.ApiClient;
-import org.openapitools.client.ApiException;
 import org.openapitools.client.api.TimeControllerApi;
 import org.openapitools.client.model.TimeResponse;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasProperty;
@@ -62,9 +52,9 @@ class TimeClientTest {
     }
 
     @Test
-    void clientTest() throws ApiException {
+    void clientTest() {
         ApiClient client = new ApiClient();
-        client.updateBaseUri("http://localhost:" + port);
+        client.setBasePath("http://localhost:" + port);
 
         String today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
         TimeResponse nowUTC = new TimeControllerApi(client).nowUTC();
